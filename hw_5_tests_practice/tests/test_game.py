f74019083fd7f1bdf0f4dc076f16727b85a3df37
Katherina Khomenko <hs.kaya.hs@gmail.com>
2021-06-07 17:52:19 +0300

    added setup method

--- hw_5_tests_practice/tests/test_game.py
+++ hw_5_tests_practice/tests/test_game.py
@@ -1,16 +1,28 @@
 import pytest
 from hw_5_tests_practice.game import TicTacToe
 
-game = TicTacToe()
-game.board[4] = "X"
+
+def setup_module(module):
+    global game_test
+    game_test = TicTacToe()
+    game_test.board[4] = "X"
 
 
 def test_available_moves():
-    assert (4 not in game.available_moves())
+    assert 4 not in game_test.available_moves()
+    assert 3 in game_test.available_moves()
+
+    with pytest.raises(AssertionError):
+        assert -1 in game_test.available_moves()
 
 
 def test_make_move():
-    assert game.make_move(4, "X") is False
-    assert game.make_move(5, "X") is True
-    assert game.board[5] != " "
+    assert game_test.make_move(4, "X") is False
+    assert game_test.make_move(5, "X") is True
+
+    with pytest.raises(IndexError):
+        assert game_test.make_move(9, "X") is False
+
+    assert game_test.board[5] != " "
+    assert game_test.board[7] == " "
 
