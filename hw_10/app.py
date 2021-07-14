5b8f2744be93699c710f27c19cb3ac8ac9bb9738
Katherina <79052817+blestem@users.noreply.github.com>
2021-07-14 23:08:35 +0300

    Add files via upload

--- hw_10/app.py
+++ hw_10/app.py
@@ -1,29 +1,25 @@
 # flask_web/app.py
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+ARITHMETIC_FUNC = {'sum': '+', 'dif': '-', 'mult': '*', 'dev': '/'}
 
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return 'Hello! It is flask calculator'
 
 
-@app.route('/calc/<int:x>/<int:y>/<string:func>')
-def calc(x, y, func):
-    if func == 'sum':
-        return render_template('calc.html', x=x, y=y, func="+", result=x + y)
-    elif func == 'dif':
-        return render_template('calc.html', x=x, y=y, func="-", result=x - y)
-    elif func == 'mult':
-        return render_template('calc.html', x=x, y=y, func="*", result=x * y)
-    elif func == 'div':
-        if y == 0:
-            return render_template('calc.html', y=y)
-        else:
-            return render_template('calc.html', x=x, y=y, func="/", result=x / y)
-    else:
-        return render_template('calc.html', func="Null")
+@app.route('/calc/<int:x>/<int:y>/<string:function>/', methods=['POST', 'GET'])
+def calc(x, y, function):
+    func = ARITHMETIC_FUNC.get(function)
+    if func:
+        try:
+            return render_template('calc.html', x=x, y=y, function=str(func), result=eval(f'{x} {func} {y}'))
+        except ZeroDivisionError:
+            return render_template('zero_division_error.html')
+    return render_template('incorrect_action.html')
 
 
 if __name__ == '__main__':
